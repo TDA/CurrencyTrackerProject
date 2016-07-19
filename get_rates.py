983c5cfde62f24f6cf30c252117c6c58e88f9ee4
Sai Chandramouli <schand31@asu.edu>
2016-07-18 19:59:35 -0700

    finished extracting currency rates

--- get_rates.py
+++ get_rates.py
@@ -1,12 +1,23 @@
 __author__ = 'saipc'
 import requests
+from bs4 import BeautifulSoup
 
 def get_current_rate(a, b):
-    url = "http://www.google.com/finance/converter?a=%s&from=%s&to=%s"
     default_amount = 1
-    print "Requesting conversion rate for "
-    r = requests.get(url%(default_amount, a, b))
-    print r
+    url = "http://www.google.com/finance/converter?a=%s&from=%s&to=%s"%(default_amount, a, b)
+    print "Requesting conversion rate for %s to %s"%(a, b)
+    r = requests.get(url)
+    currency_string = get_text(r.text, "#currency_converter_result")
+    currency_amount = float(str(currency_string).split(' ')[0])
+    return currency_amount
+
+def get_text(html, id_selector):
+    s = BeautifulSoup(html, "lxml")
+    currency_string = s.select(id_selector + ' > span')[0].get_text()
+    return currency_string
 
 if __name__ == '__main__':
-    get_current_rate('usd', 'inr')
+    a = 'usd'
+    b = 'inr'
+    currency_amount = get_current_rate(a, b)
+    print "Today's currency rate: %s to %s is %s" % (a, b, currency_amount)
